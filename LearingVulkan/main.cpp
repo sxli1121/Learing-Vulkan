38255f81986355a8f8a2d54488b568413bd88ae8
sxli <2214616934@qq.com>
2024-07-16 17:54:23 +0800

    Validation layers1

--- LearingVulkan/main.cpp
+++ LearingVulkan/main.cpp
@@ -3,14 +3,44 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-
-
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+
+const std::vector<const char*> validationLayers =
+{
+	// 开启所有的校验层
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif // NDEBUG
+
+
+// 代理函数 用来创建VkDebugUtilsMessengerCreateInfoEXT结构体
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+	const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
+{
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr)
+	{
+		return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
 
 class HelloTriangleApplication
 {
@@ -27,6 +57,8 @@ private:
 	GLFWwindow* window;
 	VkInstance instance;
 
+	VkDebugUtilsLabelEXT debugMessenger; // 回调函数句柄
+
 
 
 	void initWindow() {
@@ -41,6 +73,24 @@ private:
 	void initVulkan()
 	{
 		createInstance();
+
+		setupDebugMessenger();
+
+	}
+
+	void setupDebugMessenger()
+	{
+		if (!enableValidationLayers) return;
+
+
+		VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		createInfo.pfnUserCallback = debugCallback;
+		createInfo.pUserData = nullptr; // Optional
+
+
 	}
 
 	void mainLoop()
@@ -53,6 +103,13 @@ private:
 
 	void createInstance()
 	{
+		// 验证层
+		if (enableValidationLayers && !checkValidatonLayerSupport())
+		{
+			throw std::runtime_error("validation layers requested, but not avaliable!");
+		}
+		
+		
 		VkApplicationInfo appInfo{};  // VK的信息结构体
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "Hello Triangle";
@@ -71,9 +128,29 @@ private:
 
 		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		//createInfo.enabledExtensionCount = glfwExtensionCount;
+		//createInfo.ppEnabledExtensionNames = glfwExtensions;
 		createInfo.enabledLayerCount = 0;
+
+
+		auto extensions = getRequiredExtensions();
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+		createInfo.ppEnabledExtensionNames = extensions.data();
+
+		if (enableValidationLayers)
+		{
+			createInfo.enabledExtensionCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
+
+
+
+
+
 
 
 		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
@@ -92,6 +169,74 @@ private:
 
 		glfwTerminate();
 	}
+
+
+	bool checkValidatonLayerSupport()
+	{
+		// 获取所有校验层列表
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		// 检查valiattionLayers中的校验层能否在 获取到的列表中找到
+		for (const char* layerName : validationLayers)
+		{
+			bool layerFound = false;
+
+			for (const auto& layerProperties : availableLayers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0) // 判断两个字符串是否完全相同
+				{
+					layerFound = true;
+					break;
+				}
+			}
+			if (!layerFound)
+				return false;
+		}
+
+		return true;
+	}
+
+	// 开启校验层的时候返回调试回调信息--扩展列表
+	std::vector<const char*> getRequiredExtensions()
+	{
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+		if (enableValidationLayers)
+		{
+			extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+		}
+		return extensions;
+	}
+
+
+	// 接受调试信息的回调函数
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData)
+	{
+		std::cerr << "validation layer :" << pCallbackData->pMessage << std::endl;   // 错误流
+
+
+
+
+
+
+		return VK_FALSE;
+	}
+
+
+
+
 
 
 };
